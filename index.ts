79e1302a67292529e049b8f0fcf60f8b865b968c
kev <kev@localhost>
2020-01-17 02:29:18 +0800

    Auto commit: 2020-01-17_02:29:18

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-export * from './src'
+export enum E_eid_common {
+	base_error        = 'E0',
+	invalid_argument  = 'E1',
+	lack_config       = 'E2',
+	lack_path         = 'E3',
+	lack_argument     = 'E4',
+	permission_denied = 'E5',
+}
