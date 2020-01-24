38766543059f3ca61abbb7e725955f0dc739a97d
kev <kev@localhost>
2020-01-25 02:39:20 +0800

    Auto commit: 2020-01-25_02:39:20

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 export enum E_eid_common {
 	// Common level error code starts with 'E0'
-	base_error          = 'E00',
-	invalid_argument    = 'E01',
-	lack_config         = 'E02',
-	lack_path           = 'E03',
-	lack_argument       = 'E04',
-	permission_denied   = 'E05',
-	bad_connection      = 'E06',
-	invalid_state       = 'E07',
+	base_error         = 'E00',
+	invalid_argument   = 'E01',
+	lack_config        = 'E02',
+	lack_path          = 'E03',
+	lack_argument      = 'E04',
+	permission_denied  = 'E05',
+	bad_connection     = 'E06',
+	invalid_state      = 'E07',
 	invalid_credential = 'E08',
+	too_frequent       = 'E09'
 }
